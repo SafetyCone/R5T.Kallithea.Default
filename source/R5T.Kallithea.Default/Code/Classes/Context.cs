91e60e04c2c07acec8fbeba4d0b05414d07ddf28
MinexAutomation <minex.automation@gmail.com>
2019-12-23 00:59:01 -0800

    Added a dictionary-based IContext implementation.

---
+++ source/R5T.Kallithea.Default/Code/Classes/Context.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Kallithea.Default
+{
+    public class Context : IContext
+    {
+        protected Dictionary<string, object> KeyValuePairs { get; } = new Dictionary<string, object>();
+
+
+        public void Add(string key, object value)
+        {
+            var alreadyExists = this.Exists(key);
+            if (alreadyExists)
+            {
+                throw new Exception($"Unable to add key '{key}' since key already exists with value: {this.KeyValuePairs[key].ToString()}");
+            }
+
+            this.Set(key, value);
+        }
+
+        public bool Exists(string key)
+        {
+            var output = this.KeyValuePairs.ContainsKey(key);
+            return output;
+        }
+
+        public object Get(string key)
+        {
+            var value = this.KeyValuePairs[key];
+            return value;
+        }
+
+        public bool Remove(string key)
+        {
+            var output = this.KeyValuePairs.Remove(key);
+            return output;
+        }
+
+        public void Set(string key, object value)
+        {
+            this.KeyValuePairs[key] = value;
+        }
+    }
+}
